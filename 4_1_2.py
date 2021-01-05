c57b17b101ea853b0239c549ee023b8ac2178db0
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:43:27 +0100

    Update 4_1_2.py

--- 4_1_2.py
+++ 4_1_2.py
@@ -1,5 +1,14 @@
+  
 """
-Simple predictions + MSE/MAE
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 4: Sequences and Prediction
+
+Aim: Mean Squared Error and Mean Absolute Error
+
 """
 
 import numpy as np
