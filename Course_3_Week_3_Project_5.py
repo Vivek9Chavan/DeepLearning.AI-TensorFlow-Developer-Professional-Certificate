051a4675e7fc666f9891c3e9e3be01af02490131
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:33:24 +0100

    Update and rename 3_3_5.py to Course_3_Week_3_Project_5.py

--- Course_3_Week_3_Project_5.py
+++ Course_3_Week_3_Project_5.py
@@ -1,6 +1,14 @@
 """
-utf-8 encoded training file
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 3: Sequence Models
+
+Aim: Monitoring validation losses
 """
+
 import json
 import tensorflow as tf
 import csv
