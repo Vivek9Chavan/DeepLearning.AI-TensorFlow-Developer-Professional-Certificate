762569853780e1cc76faa31f93739ba3b9a8b512
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:06:23 +0100

    Update and rename 2_3_2.py to Course_2_Week_3_Project_2.py

--- Course_2_Week_3_Project_2.py
+++ Course_2_Week_3_Project_2.py
@@ -1,4 +1,16 @@
 
+"""
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 3: Transfer Learning
+
+Aim: Week 3 Test
+"""
+
+
 import os
 import tensorflow as tf
 from tensorflow.keras import layers
