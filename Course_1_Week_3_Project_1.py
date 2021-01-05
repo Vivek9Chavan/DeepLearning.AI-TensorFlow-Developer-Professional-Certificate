23779e88c9f489830d974b803fde88837d035ab3
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 05:53:06 +0100

    Update Course_1_Week_3_Project_1.py

--- Course_1_Week_3_Project_1.py
+++ Course_1_Week_3_Project_1.py
@@ -5,7 +5,7 @@ This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certi
 All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
 Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
 
-Week 1: Enhancing Vision with Convolutional Neural Networks
+Week 2: Enhancing Vision with Convolutional Neural Networks
 
 Aim: Creating manual convolutions and filters:
 
