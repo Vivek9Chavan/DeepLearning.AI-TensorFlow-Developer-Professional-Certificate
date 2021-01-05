8492bfeadf620593d660d63b7c78448f501921dd
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 05:50:56 +0100

    Update and rename 1_4_3.py to Course_1_Week_4_Project_3.py

--- Course_1_Week_4_Project_3.py
+++ Course_1_Week_4_Project_3.py
@@ -1,16 +1,14 @@
 """
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
 
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 1: Using Real-world Images
+
+Aim: Binary classification- Happy or sad faces
 """
 
-"""
-In this exercise you'll try to build a neural network that predicts the price of a house according to a simple formula.
-
-So, imagine if house pricing was as easy as a house costs 50k + 50k per bedroom, so that a 1 bedroom house costs 100k, a 2 bedroom house costs 150k etc.
-
-How would you create a neural network that learns this relationship so that it would predict a 7 bedroom house as costing close to 400k etc.
-
-Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...it might be better to create something that predicts the number 4, and then your answer is in the 'hundreds of thousands' etc.
-"""
 import tensorflow as tf
 import os
 import zipfile
@@ -72,3 +70,4 @@ def train_happy_sad_model():
     return history.history["acc"][-1]
 
 train_happy_sad_model()
+
