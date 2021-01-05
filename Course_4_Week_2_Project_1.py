82b1420b6021c551967c7bfdae9a9db39c64d544
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:46:13 +0100

    Update and rename 4_2_1.py to Course_4_Week_2_Project_1.py

--- Course_4_Week_2_Project_1.py
+++ Course_4_Week_2_Project_1.py
@@ -1,6 +1,15 @@
 """
-Preparing batches out of simple datasets
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 2: Deep Neural Networks for Time Series
+
+Aim: Creating Batch datasets
+
 """
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
