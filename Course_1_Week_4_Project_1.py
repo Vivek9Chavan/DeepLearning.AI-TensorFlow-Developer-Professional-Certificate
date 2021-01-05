675c464f500876477268a0845766850fdbbc3525
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 05:48:11 +0100

    Update and rename 1_4_1.py to Course_1_Week_4_Project_1.py

--- Course_1_Week_4_Project_1.py
+++ Course_1_Week_4_Project_1.py
@@ -1,4 +1,15 @@
 
+"""
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 1: Using Real-world Images
+
+Aim: Binary classification using computer generated images and predicting on real world images
+"""
+
 import os
 import zipfile
 from os import path, getcwd, chdir
