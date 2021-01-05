483c31c73519bdb3399df7e25d93e0e6165a3f59
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:12:29 +0100

    Update and rename 3_1_2.py to Course_3_Week_1_Project_2.py

--- Course_3_Week_1_Project_2.py
+++ Course_3_Week_1_Project_2.py
@@ -1,3 +1,13 @@
+"""
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 1: Sentiment in text
+
+Aim: Tokenization and padding sequences
+"""
 
 import tensorflow as tf
 from tensorflow import keras
