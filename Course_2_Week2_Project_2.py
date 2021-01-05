087bbc66efcec866397d9535bc7de94280b39968
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:01:11 +0100

    Update and rename 2_2_2.py to Course_2_Week2_Project_2.py

--- Course_2_Week2_Project_2.py
+++ Course_2_Week2_Project_2.py
@@ -1,4 +1,15 @@
 
+"""
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 2: Data Augmentation
+
+Aim: Horses or Humans Dataset
+"""
+
 import os
 import zipfile
 from os import path, getcwd, chdir
