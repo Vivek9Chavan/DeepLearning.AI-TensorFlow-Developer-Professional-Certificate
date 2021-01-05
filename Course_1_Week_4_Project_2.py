a9bef49cd5ad1816f875528abfbbe1794a705c9f
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 05:54:56 +0100

    Update Course_1_Week_4_Project_2.py

--- Course_1_Week_4_Project_2.py
+++ Course_1_Week_4_Project_2.py
@@ -4,7 +4,7 @@ This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certi
 All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
 Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
 
-Week 1: Using Real-world Images
+Week 4: Using Real-world Images
 
 Aim: CNNs-Binary classification using computer generated images and predicting on real world images
 """
