7c74a7cfdf516f3729a51e31b7129466ade09d59
Vivek Chavan <57413096+Vivek9Chavan@users.noreply.github.com>
2021-01-05 06:34:44 +0100

    Update and rename 3_4_1.py to Course_3_Week_4_Project_1.py

--- Course_3_Week_4_Project_1.py
+++ Course_3_Week_4_Project_1.py
@@ -1,4 +1,15 @@
 
+"""
+This is is a part of the DeepLearning.AI TensorFlow Developer Professional Certificate offered on Coursera.
+
+All copyrights belong to them. I am sharing this work here to showcase the projects I have worked on
+Course: Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning
+
+Week 4: Sequence models and literature
+
+Aim: Simple Irish text
+"""
+
 import tensorflow as tf
 import numpy as np
 
